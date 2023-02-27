9df055cee2d69baf3d1975b4a02a11f001afd9ce
samidgiraldello <samidgiraldello9@gmail.com>
2023-02-27 10:26:49 -0300

    samid

--- App.tsx
+++ App.tsx
@@ -1,85 +1,22 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, Text, Image, TouchableOpacity, Alert } from 'react-native'
-import logo from "./assets/logo.png"
-import Toggle from './Toggle'
+import React, { SetStateAction, useState, Dispatch } from 'react';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
 
-export default function App() {
-  const [flexDirection, setFlexDirection] = useState('row')
-  const [justifyContent, setJustifyContent] = useState('center')
-  const [alignItems, setAlignItems] = useState('center')
-  const layoutStyle = { flexDirection, justifyContent, alignItems }
-
-  const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
-  const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
-
-  return (
-    <View style={styles.container}>
-      <Toggle
-        label={'Primary axis (flexDirection)'}
-        value={flexDirection}
-        options={flexDirectionOptions}
-        onChange={(option) => {
-          setFlexDirection(option)
-        }}
-      />
-      <Toggle
-        label={`${primaryAxis} distribution (justifyContent)`}
-        value={justifyContent}
-        options={justifyContentOptions}
-        onChange={(option) => {
-          setJustifyContent(option)
-        }}
-      />
-      <Toggle
-        label={`${secondaryAxis} alignment (alignItems)`}
-        value={alignItems}
-        options={alignItemsOptions} 
-        onChange={(option) => {
-          setAlignItems(option)
-        }}
-      />
-      <Text>Olá, você deseja fazer um pedido</Text>
-      <TouchableOpacity onPress={() => {
-        Alert.alert('Clicou!')
-      }}>
-        <Image source={logo} style={styles.img} />
-      </TouchableOpacity>
-      <Image source={logo} style={styles.img} />
-      <View style={[styles.layout, layoutStyle]}>
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-      </View>
-    </View>
-  )
+export interface IPage {
+  setPageI: Dispatch<SetStateAction<number>>
 }
 
-const flexDirectionOptions = ['row', 'column']
-const justifyContentOptions = [
-  'flex-start',
-  'center',
-  'flex-end',
-  'space-around',
-  'space-between',
-]
-const alignItemsOptions = ['flex-start', 'center', 'flex-end', 'stretch']
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  layout: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.05)',
-  },
-  box: {
-    padding: 25,
-    backgroundColor: '#3B6CD4',
-    margin: 5,
-  },
-  img: {
-    width: 50,
-    height: 50
+export default function App() {
+ const [page, setPage] = useState(1)
 
-  }
-})
+  return (
+    <>
+     {
+      page == 1 ?
+        (<Page1 setPageI={setPage} />) :
+        (<Page2 setPageI={setPage} />)
+     }
+     </>
+  )
+}
